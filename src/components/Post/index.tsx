f65dca2494fd0f235e0479e66fd8fd6be5a9ca9f
Fred <frgauz@gmail.com>
2021-02-15 05:25:48 -0800

    Add padding and crude limiting to text chars

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -6,6 +6,7 @@ import { colors } from '../../styles/colors';
 const Card = styled.div`
   width: 300px;
   height: 300px;
+  padding: 10px;
 `;
 
 const Title = styled.h1`
@@ -25,23 +26,24 @@ const Text = styled.p`
 
 interface IPost {}
 export const Post = (props: IPost) => {
+  const text = `Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+  accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+  illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+  explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+  odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+  voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
+  quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
+  eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+  voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
+  corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+  Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
+  quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
+  voluptas nulla pariatur?`
   return (
     <Card>
       <Title>Main Title</Title>
       <Text>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
-        quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
-        eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-        voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
-        corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-        Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
-        quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-        voluptas nulla pariatur?
+        {text.slice(0,450)}...
       </Text>
     </Card>
   );
