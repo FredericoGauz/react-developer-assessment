b5f18e4b5c742243047463faeeb328cace87a5b1
Fred <frgauz@gmail.com>
2021-02-15 08:22:36 -0800

    Use dinamic props in Post

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-
+import { prune } from 'voca';
 import styled from 'styled-components';
 import { colors } from '../../styles/colors';
+import { IPost } from '../../types/post.interface';
 import { Link } from '../Link';
 
 const Card = styled.div`
@@ -23,7 +24,7 @@ const Title = styled.h1`
   }
 `;
 
-const Text = styled.p`
+const Text = styled.summary`
   font-family: 'Roboto', serif;
   font-weight: 300;
   line-height: 1.7em;
@@ -32,31 +33,21 @@ const Text = styled.p`
   color: ${colors.darkGray};
 `;
 
-interface IPost {}
-export const Post = (props: IPost) => {
-  const text = `Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-  accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-  illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-  explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-  odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-  voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
-  quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
-  eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-  voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
-  corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-  Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
-  quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-  voluptas nulla pariatur?`;
+interface IPostProps {
+  post: IPost;
+}
+export const Post = ({ post }:IPostProps) => {
+  const {title, summary} = post;
   return (
     <Card>
       <Title>
-        <Link to="/post">Main Title</Link>
+        <Link to="/post">{prune(title, 40)}</Link>
       </Title>
       <Text>
-        {text.slice(0, 450)}
-        <span>
+        {prune(summary, 450)}
+        {summary.length > 450 && <span>
           <Link to="/post">...</Link>
-        </span>
+        </span>}
       </Text>
     </Card>
   );
