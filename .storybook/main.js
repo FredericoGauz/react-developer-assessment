1616588cc84dbe41bedc310147e2dbf3b311b80a
Fred <frgauz@gmail.com>
2021-02-15 13:33:07 -0800

    Remove unnecessary story paths

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,6 @@ module.exports = {
   stories: [
     '../src/**/*.stories.mdx',
     '../src/**/*.stories.@(js|jsx|ts|tsx)',
-    '../src/components/**/*.stories.mdx',
-    '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-links',
