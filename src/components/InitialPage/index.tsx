0ef147477f9d2ab5b4b20e8d056167835eb16189
Fred <frgauz@gmail.com>
2021-02-15 06:28:43 -0800

    Add title to InitialPage

--- src/components/InitialPage/index.tsx
+++ src/components/InitialPage/index.tsx
@@ -4,13 +4,22 @@ import { Layout } from '../Layout';
 import data from '../../mock/data.json';
 import { PostList } from '../PostList';
 import { IPost } from '../../types/post.interface';
+import styled from 'styled-components';
+import { colors } from '../../styles/colors';
 
+const HeaderTitle = styled.h1`
+  font-family: 'Lora', serif;
+  font-weight: 700;
+  color: ${colors.blackGray};
+  font-size:4em;
+`;
 
 interface IInitialPage {}
 export const InitialPage = (props: IInitialPage) => {
   const posts:IPost[] = data.posts;
   return (
     <Layout>
+      <HeaderTitle>World News</HeaderTitle>
       <PostList posts={posts.slice(0,12)} />
     </Layout>
   );
